d8df92314bb22d68ee3e22c58e8f1cc749ad5ccd
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-08-29 19:31:50 -0500

    implements wwise beat service
    adds implementation for wwise beat service to synchronize with the wwise timestamp, generates sound banks and events in wwise for stage 1

--- Assets/Scripts/Source/Audio/WwiseBeatService.cs
+++ Assets/Scripts/Source/Audio/WwiseBeatService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using AK.Wwise;
 
 namespace BattleRoyalRhythm.Audio
 {
@@ -9,8 +11,49 @@ namespace BattleRoyalRhythm.Audio
     /// </summary>
     public sealed class WwiseBeatService : BeatService
     {
-        public override float CurrentInterpolant => throw new NotImplementedException();
+        private float lastFixedTime = 0f;
+        private float lastInterpolant = 0f;
+        private float currentInterpolant;
+        public override float CurrentInterpolant => currentInterpolant;
+
+        #region Beat Interval State
+        #endregion
 
         public override event BeatElapsedHandler BeatElapsed;
+
+        [SerializeField] private int playPosition = 1;
+
+        private uint beatMusicID;
+
+        private float millisPerBeat = 1f;
+
+        public void SetBeatFromEvent(string eventName, float bpm)
+        {
+            millisPerBeat = 60000f / bpm;
+            beatMusicID = AkSoundEngine.PostEvent(
+                eventName, gameObject,
+                (uint)AkCallbackType.AK_EnableGetSourcePlayPosition,
+                WwiseCallback, null);
+        }
+        // This callback is needed for Wwise to wwork.
+        private void WwiseCallback(object in_cookie, AkCallbackType in_type, AkCallbackInfo in_info) { }
+
+        private void FixedUpdate()
+        {
+            AkSoundEngine.GetSourcePlayPosition(beatMusicID, out playPosition);
+
+            currentInterpolant = (playPosition % millisPerBeat) / millisPerBeat;
+
+
+            // Check if a beat has elapsed.
+            if (currentInterpolant < lastInterpolant)
+            {
+                // Increment the elapsed beat and
+                // notify listeners of the service.
+                BeatElapsed?.Invoke(Mathf.Lerp(lastFixedTime, Time.fixedTime, Mathf.InverseLerp(lastInterpolant, currentInterpolant + 1f, 1f)));
+            }
+            lastInterpolant = currentInterpolant;
+            lastFixedTime = Time.fixedDeltaTime;
+        }
     }
 }

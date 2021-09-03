7181eac3c1bae46b002c8e338cc274193835daca
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-09-03 18:53:57 -0500

    first pass repulsor
    adds shell for repulsor property values

--- Assets/Scripts/Source/GridActors/Player/AbilityRepulsor.cs
+++ Assets/Scripts/Source/GridActors/Player/AbilityRepulsor.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace BattleRoyalRhythm.GridActors.Player
 {
     public sealed class AbilityRepulsor : ActorAbility
     {
+        [SerializeField][Min(0)] private int repulsorJumps = 1;
+        [SerializeField][Min(1)] private int repulsionRadius = 1;
+        [SerializeField][Min(1)] private int repulsorKnockback = 2;
 
+        public override void StartUsing(int beatCount)
+        {
+            
+        }
     }
 }

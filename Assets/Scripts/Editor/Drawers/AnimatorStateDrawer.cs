a8162b8fbbf8214000d151e553b1b14727ea7b78
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-11-02 16:42:16 -0500

    adds documentation for AnimatorState
    adds comments and doc comments for the usage and implementation of AnimatorState

--- Assets/Scripts/Editor/Drawers/AnimatorStateDrawer.cs
+++ Assets/Scripts/Editor/Drawers/AnimatorStateDrawer.cs
@@ -1,65 +1,73 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using Tools;
-using System;
 
 namespace BattleRoyalRhythm.UnityEditor.Drawers
 {
+    // NOTE an interface is used here because property drawers
+    // cannot currently support generic typeargs in classes.
+    // Serialized properties can be arbitrarily retrieved,
+    // so it is not a problem that the implementation is not specified.
+    /// <summary>
+    /// Handles drawing for the binding of a given enum
+    /// to the string parameters inside an animation state
+    /// machine.
+    /// </summary>
     [CustomPropertyDrawer(typeof(IAnimatorState), true)]
     public sealed class AnimatorStateDrawer : PropertyDrawer
     {
-
+        #region Drawer State
         private bool isExpanded;
-
         public AnimatorStateDrawer()
         {
             isExpanded = false;
         }
-
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => 0f;
-
+        #endregion
+        #region Drawer Implementation
+        // 0 is returned because EditorGUILayout makes its own
+        // space and the default property height is unused.
+        public override sealed float GetPropertyHeight(SerializedProperty property, GUIContent label) => 0f;
         public override sealed void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-
+            // Wrap this property in a drop down.
             isExpanded = EditorGUILayout.Foldout(isExpanded, label);
-
             if (isExpanded)
             {
                 EditorGUI.indentLevel++;
-                SerializedProperty animatorProp = property.FindPropertyRelative("boundAnimator");
-                EditorGUILayout.PropertyField(animatorProp);
 
+                // Grab the properties. (See AnimatorState.cs)
+                SerializedProperty animatorProp = property.FindPropertyRelative("boundAnimator");
                 SerializedProperty bindingsProp = property.FindPropertyRelative("bindings");
                 SerializedProperty stateProp = property.FindPropertyRelative("state");
+                string[] names = stateProp.enumDisplayNames;
 
-                string[] names = stateProp.enumNames;
-                string[] displayNames = stateProp.enumDisplayNames;
-
-                // Resize the array in case the enum has changed.
+                // Resize the array of bindings if the enum has changed.
                 int size = bindingsProp.arraySize;
-                int resize = names.Length - size;
-                if (resize > 0)
-                    for (int i = 0; i < resize; i++)
+                int elementsToAdd = names.Length - size;
+                if (elementsToAdd > 0)
+                    for (int i = 0; i < elementsToAdd; i++)
                         bindingsProp.InsertArrayElementAtIndex(size + i);
-                else if (resize < 0)
-                    for (int i = 0; i < Mathf.Abs(resize); i++)
+                else if (elementsToAdd < 0)
+                    for (int i = 0; i < Mathf.Abs(elementsToAdd); i++)
                         bindingsProp.DeleteArrayElementAtIndex(size - 1 - i);
 
+                // Create fields for the animator reference and each
+                // enum bound bool parameter in the animator.
+                EditorGUILayout.PropertyField(animatorProp);
                 EditorGUILayout.LabelField(new GUIContent("Bool Parameters"));
-
                 for (int i = 0; i < names.Length; i++)
                 {
                     SerializedProperty bindingProp = bindingsProp.GetArrayElementAtIndex(i);
                     SerializedProperty nameProp = bindingProp.FindPropertyRelative("boolName");
                     SerializedProperty enumProp = bindingProp.FindPropertyRelative("enumValue");
-
-                    EditorGUILayout.PropertyField(nameProp, new GUIContent(displayNames[i]));
+                    EditorGUILayout.PropertyField(nameProp, new GUIContent(names[i]));
+                    // This ensures the proper enum value is bound.
+                    // (this is redundant after one pass).
                     enumProp.enumValueIndex = i;
                 }
 
                 EditorGUI.indentLevel--;
             }
         }
-
+        #endregion
     }
 }
--- Assets/Scripts/Source/AnimatorState.cs
+++ Assets/Scripts/Source/AnimatorState.cs
@@ -1,33 +1,70 @@
 using System;
 using UnityEngine;
 
+// This interface is needed for the drawer to
+// be applicable to any typeargument for the Enum.
+// Typeargs in drawer target types are not supported.
+/// <summary>
+/// Identifies the standard implementation of
+/// AnimatorState(TEnum).<br/>
+/// IMPORTANT: This interface is NOT meant to be used outside of
+/// the standard implementation.
+/// </summary>
 public interface IAnimatorState { }
 
+/// <summary>
+/// Binds an enum directly to a corresponding set of bool parameters
+/// inside a Unity Animator. Use the State property to automatically
+/// update the appropriate animator parameters. Enums can be marked
+/// with the [Flags] attribute to toggle multiple parameters at once.
+/// </summary>
+/// <typeparam name="TEnum">The enum to use for state. Each enum value
+/// correpsonds to a bool parameter in the Unity Animator.</typeparam>
 [Serializable]
 public sealed class AnimatorState<TEnum> : IAnimatorState
     where TEnum : Enum
 {
+    #region Local Data Structure
+    // This data structure stores the mappings from
+    // the enum to the hash value. Hash value is calculated
+    // once at evaluation time from the bool name provided by
+    // the inspector.
     [Serializable]
     private sealed class AnimatorBinding
     {
+        /// <summary>
+        /// The name of the boolean animator parameter.
+        /// </summary>
         [SerializeField] public string boolName;
+        /// <summary>
+        /// The hash ID of the animator paramater.
+        /// </summary>
         [SerializeField] public int hashValue;
+        /// <summary>
+        /// The bound enum value that enables this parameter.
+        /// </summary>
         [SerializeField] public TEnum enumValue;
     }
-
-
+    #endregion
+    #region Serialization Structure
+    [Tooltip("The animator that this state drives.")]
     [SerializeField] private Animator boundAnimator = null;
-
-    [SerializeField] private TEnum state = default;
-
     [SerializeField] private AnimatorBinding[] bindings = null;
-
-
+    // TODO this should be exposed as a readonly field in the inspector.
+    [SerializeField] private TEnum state = default;
+    #endregion
+    #region Initialization
     public AnimatorState()
     {
+        // This will be resized and filled by the drawer.
         bindings = new AnimatorBinding[0];
     }
-
+    #endregion
+    #region Runtime Implementation
+    /// <summary>
+    /// The current state of the animator. Updating this
+    /// directly will cause the animator to update.
+    /// </summary>
     public TEnum State
     {
         get => state;
@@ -41,7 +78,6 @@ public sealed class AnimatorState<TEnum> : IAnimatorState
                 boundAnimator.SetBool(binding.hashValue, state.HasFlag(binding.enumValue));
         }
     }
-
     private void CheckGenerateHashes()
     {
         // If the hash values have not been generated,
@@ -51,4 +87,5 @@ public sealed class AnimatorState<TEnum> : IAnimatorState
             foreach (AnimatorBinding binding in bindings)
                 binding.hashValue = Animator.StringToHash(binding.boolName);
     }
+    #endregion
 }

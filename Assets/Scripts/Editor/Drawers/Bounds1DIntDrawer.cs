7d62be15237fe546249eba1604e57d80aba1471b
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-11-26 20:23:47 -0600

    fixes drawer for BoundsInt1D
    previously this drawer would not function drawing serialized fields in ScriptableObjects, this has been fixed by removing the use of EditorGUILayout

--- Assets/Scripts/Editor/Drawers/Bounds1DIntDrawer.cs
+++ Assets/Scripts/Editor/Drawers/Bounds1DIntDrawer.cs
@@ -11,28 +11,56 @@ namespace BattleRoyalRhythm.UnityEditor.Drawers
     [CustomPropertyDrawer(typeof(Bounds1DInt))]
     public sealed class Bounds1DIntDrawer : PropertyDrawer
     {
-        // TODO this is a hotfix for the quickly
-        // implemented layout code below.
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => 0f;
+        #region Drawer Initialization + Styling
+        private readonly GUIStyle minLabelStyle, maxLabelStyle, minFieldStyle, maxFieldStyle;
+        private readonly float minMaxLabelWidth, fieldWidth, gapWidth;
+        public Bounds1DIntDrawer()
+        {
+            // Precalculate styles and layout values.
+            minLabelStyle = new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleRight };
+            maxLabelStyle = new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft };
+            minFieldStyle = new GUIStyle(GUI.skin.textField) { alignment = TextAnchor.MiddleRight };
+            maxFieldStyle = new GUIStyle(GUI.skin.textField) { alignment = TextAnchor.MiddleLeft };
+            // Widths are based on proportions of line height.
+            minMaxLabelWidth = EditorGUIUtility.singleLineHeight * 2f;
+            fieldWidth = EditorGUIUtility.singleLineHeight * 3f;
+            gapWidth = EditorGUIUtility.singleLineHeight * 0.5f;
+        }
+        #endregion
         #region Drawer Implementation
         public override sealed void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            // Calculate the rects to draw.
+            Rect labelRect = new Rect(position) { width = EditorGUIUtility.labelWidth };
+            Rect minLabelRect = new Rect(labelRect) { width = minMaxLabelWidth, x = labelRect.xMax };
+            Rect minRect = new Rect(minLabelRect) { width = fieldWidth, x = minLabelRect.xMax };
+            Rect maxRect = new Rect(minRect) { width = fieldWidth, x = minRect.xMax + gapWidth };
+            Rect maxLabelRect = new Rect(maxRect) { width = minMaxLabelWidth, x = maxRect.xMax };
+            // Get the properties to effect.
             SerializedProperty min = property.FindPropertyRelative("min");
             SerializedProperty max = property.FindPropertyRelative("max");
-            // TODO research vector2int to figure out how to properly draw
-            // this including the labels.
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(label);
-            EditorGUIUtility.labelWidth = 100.0f;
-            EditorGUILayout.PropertyField(min);
-            EditorGUILayout.PropertyField(max);
-            GUILayout.FlexibleSpace();
-            EditorGUILayout.EndHorizontal();
+            // Draw the properties.
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.LabelField(labelRect, label);
+            EditorGUI.LabelField(minLabelRect, min.displayName, minLabelStyle);
+            int newMin = EditorGUI.IntField(minRect, min.intValue, minFieldStyle);
+            int newMax = EditorGUI.IntField(maxRect, max.intValue, maxFieldStyle);
+            EditorGUI.LabelField(maxLabelRect, max.displayName, maxLabelStyle);
             // Validate the values of the range.
-            int minValue = min.intValue;
-            int maxValue = max.intValue;
-            if (minValue > maxValue)
-                min.intValue = maxValue;
+            // Assume that only one value has changed, we always want the value
+            // adjusting to force the other end of the range.
+            if (newMin != min.intValue)
+            {
+                if (newMin > newMax)
+                    newMax = newMin;
+            }
+            else if (newMax != max.intValue)
+                if (newMax < newMin)
+                    newMin = newMax;
+            // Update the values.
+            min.intValue = newMin;
+            max.intValue = newMax;
+            EditorGUI.EndProperty();
         }
         #endregion
     }

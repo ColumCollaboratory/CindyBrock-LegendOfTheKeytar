b08ebb13c25f394d52969f69bd6288dfc14f8d69
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-12-03 16:45:01 -0600

    adds percent drawer attribute
    adds a custom drawer for drawing a percent from 0-100 that is stored internally as a float between 0-1

---
+++ Assets/Scripts/Editor/Drawers/PercentDrawer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Tools;
+
+namespace BattleRoyalRhythm.UnityEditor.Drawers
+{
+    /// <summary>
+    /// Draws a float in the inspector that ranges
+    /// from 0-1 as a percent between 0-100.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(PercentAttribute))]
+    public sealed class PercentDrawer : PropertyDrawer
+    {
+        #region Drawer Implementation
+        public override sealed void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Rect prop = new Rect(position);
+            prop.width -= EditorGUIUtility.singleLineHeight;
+
+            float startFieldX = EditorGUIUtility.labelWidth + GUI.skin.textField.padding.left;
+
+            Rect fill = new Rect(position)
+            {
+                x = position.x + startFieldX,
+                width = (prop.width - startFieldX) * property.floatValue
+            };
+            Rect rhs = new Rect(fill)
+            {
+                x = prop.xMax,
+                width = EditorGUIUtility.singleLineHeight
+            };
+            EditorGUI.DrawRect(fill, new Color(0.5f, 0.1f, 0.1f));
+
+            GUIStyle style = new GUIStyle(GUI.skin.textField)
+            {
+                alignment = TextAnchor.MiddleRight
+            };
+
+            Color prior = GUI.backgroundColor;
+            GUI.backgroundColor = new Color(1f, 1f, 1f, 0.5f);
+            float newValue = EditorGUI.FloatField(prop, label, property.floatValue * 100f, style) / 100f;
+            EditorGUI.LabelField(rhs, "%");
+            GUI.backgroundColor = prior;
+
+            newValue = Mathf.Clamp01(newValue);
+            property.floatValue = newValue;
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Tools/PercentAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Tools
+{
+    /// <summary>
+    /// Marks a floating point field (from 0 to 1) to be displayed as a percent.
+    /// </summary>
+    public sealed class PercentAttribute : PropertyAttribute { }
+}

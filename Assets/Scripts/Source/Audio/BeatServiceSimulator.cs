49d8fccf04b624dc5c083be01c9a526be37e0b4a
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-12-07 17:10:44 -0600

    adds an action debugger class
    adds a class that allows beat actions to be tested in isolation, in addition to a beat service simulator that can be used to watch actions execute at different speeds; editor pause functionality added around beat rollover edge cases

---
+++ Assets/Scripts/Source/Audio/BeatServiceSimulator.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using Tools;
+
+namespace BattleRoyalRhythm.Audio
+{
+    /// <summary>
+    /// Simulates a beat service timing mechanism. This is
+    /// used for testing only and is not directly bound to
+    /// any audio solution.
+    /// </summary>
+    public sealed class BeatServiceSimulator : MonoBehaviour
+    {
+        #region Inspector Fields
+        [Tooltip("The BPM to simulate.")]
+        [SerializeField][Min(1f)] private float bpm = 140f;
+        [Tooltip("The speed of the simulation. Can be slowed to closely observe behaviour.")]
+        [SerializeField][Percent] private float simulationSpeed = 1f;
+        [Tooltip("Pauses the editor the instant before beat elapsed is called.")]
+        [SerializeField] private bool pauseEditorBeforeBeatElapsed = false;
+        [Tooltip("Pauses the editor the instant after beat elapsed is called.")]
+        [SerializeField] private bool pauseEditorAfterBeatElapsed = false;
+        #endregion
+        #region Simulated Events
+        /// <summary>
+        /// Called when a simulated beat has elapsed.
+        /// </summary>
+        public event BeatElapsedHandler BeatElapsed;
+        #endregion
+        #region Simulated Properties
+        /// <summary>
+        /// The current interpolant from the prior beat to the next beat.
+        /// </summary>
+        public float CurrentInterpolant { get; private set; }
+        #endregion
+        #region Local Timer Fields
+        private bool hasInitialized;
+        private float secondsPerBeat;
+        private float timeElapsed;
+        #endregion
+        #region Beat Timer Implementation
+        private void LateUpdate()
+        {
+            if (!hasInitialized)
+            {
+                // Force the first beat early, but make
+                // sure that Awake and Start have enabled.
+                timeElapsed = secondsPerBeat;
+                secondsPerBeat = 1f / (bpm / 60f);
+                hasInitialized = true;
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (hasInitialized)
+            {
+                // Step time and check for an elapsed beat.
+                timeElapsed += Time.fixedDeltaTime * simulationSpeed;
+                if (timeElapsed > secondsPerBeat)
+                {
+                    // When debugging the edge state can be checked
+                    // before and after this event is called.
+                    if (pauseEditorBeforeBeatElapsed) Debug.Break();
+                    BeatElapsed?.Invoke(Time.fixedTime - (timeElapsed - secondsPerBeat));
+                    if (pauseEditorAfterBeatElapsed) Debug.Break();
+                    timeElapsed -= secondsPerBeat;
+                }
+                // Update the simulated interpolant.
+                CurrentInterpolant = timeElapsed / secondsPerBeat;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Source/GridActors/Behaviours/ActionDebugger.cs
@@ -0,0 +1,102 @@
+using BattleRoyalRhythm.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+namespace BattleRoyalRhythm.GridActors.Behaviours
+{
+    /// <summary>
+    /// On the first beat elapsed, attempts to execute
+    /// the specified action given the actor context.
+    /// Used to debug the execution of actions.
+    /// </summary>
+    public sealed class ActionDebugger : GridActor
+    {
+        [Header("Action Debug")]
+        [Tooltip("The target action to debug.")]
+        [SerializeField] private ActorAction action = null;
+        [Tooltip("The beat service simulator.")]
+        [SerializeField] private BeatServiceSimulator beatServiceSimulator = null;
+        [Header("Current Action State")]
+        [Tooltip("Whether the action was possible.")]
+        [SerializeField][ReadonlyField] private bool isPossible = false;
+        [Tooltip("Whether executing the action would have any effect.")]
+        [SerializeField][ReadonlyField] private bool hasEffect = false;
+        [Tooltip("The total movement from this action.")]
+        [SerializeField][ReadonlyField] public Vector2Int predictedEndpointDelta = Vector2Int.zero;
+        [Tooltip("Whether the action can currently be interrupted.")]
+        [SerializeField][ReadonlyField] public bool isInterruptible = false;
+        [Tooltip("How many beats are left on this action.")]
+        [SerializeField][ReadonlyField] public float beatsLeft = 0f;
+
+        private IActionContext context;
+        private bool executingAction;
+        private Vector2 lastMovementDelta;
+
+
+
+
+        private void Start()
+        {
+            executingAction = false;
+            beatServiceSimulator.BeatElapsed += OnFirstBeatElapsed;
+        }
+        protected override sealed void OnDestroy()
+        {
+            base.OnDestroy();
+            beatServiceSimulator.BeatElapsed -= OnFirstBeatElapsed;
+            beatServiceSimulator.BeatElapsed -= OnFollowingBeatsElapsed;
+        }
+
+        private void OnFirstBeatElapsed(float beatTime)
+        {
+            context = action.GetActionContext(this);
+            if (context.IsPossible)
+            {
+                executingAction = true;
+                lastMovementDelta = Vector2.zero;
+                beatServiceSimulator.BeatElapsed += OnFollowingBeatsElapsed;
+            }
+            beatServiceSimulator.BeatElapsed -= OnFirstBeatElapsed;
+            UpdateDebugFields();
+        }
+
+        private void OnFollowingBeatsElapsed(float beatTime)
+        {
+            action.AdvanceActionBeat(ref context);
+            if (context.BeatsLeft <= 0)
+            {
+                executingAction = false;
+                beatServiceSimulator.BeatElapsed -= OnFollowingBeatsElapsed;
+                // Finalize the position of the actor.
+                Vector2 movementDelta = action.GetActionDelta(ref context, 1f);
+                World.TranslateActor(this, movementDelta - lastMovementDelta);
+            }
+            UpdateDebugFields();
+        }
+
+        private void UpdateDebugFields()
+        {
+            // Update the displayed debug values.
+            isPossible = context.IsPossible;
+            hasEffect = context.HasEffect;
+            predictedEndpointDelta = context.PredictedEndpointDelta;
+            isInterruptible = context.IsInterruptible;
+            beatsLeft = context.BeatsLeft;
+        }
+
+        private void Update()
+        {
+            if (executingAction)
+            {
+                beatsLeft = context.BeatsLeft - beatServiceSimulator.CurrentInterpolant;
+                // Update the position of the actor.
+                Vector2 movementDelta = action.GetActionDelta(
+                    ref context, beatServiceSimulator.CurrentInterpolant);
+                World.TranslateActor(this, movementDelta - lastMovementDelta);
+                lastMovementDelta = movementDelta;
+            }
+        }
+    }
+}

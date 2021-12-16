be30a86fa6e0bd8b901e159c33177c5d331f9bc5
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-12-16 17:11:15 -0600

    attempts to add new soundtrack state
    squash me

--- Assets/Scripts/Source/UI/WwiseJukebox.cs
+++ Assets/Scripts/Source/UI/WwiseJukebox.cs
@@ -45,6 +45,9 @@ namespace BattleRoyalRhythm.UI
             }
         }
 
+        public float CurrentTrackDuration { get; }
+        public float CurrentTrackTime { get; set; }
+
         protected abstract void OnLoopModeChanged();
         public abstract void Play();
         public abstract void SetTrack(int trackID);
@@ -53,28 +56,45 @@ namespace BattleRoyalRhythm.UI
         public abstract void ToggleTrackAutoplay(int trackID, bool isAutoPlay);
     }
 
+    [Serializable]
+    public struct SoundtrackMetaData
+    {
+        [Tooltip("The signal to be sent to Wwise to trigger this track.")]
+        public string wwiseStateName;
+        [Header("User Visible Info")]
+        [Tooltip("The name of the track.")]
+        public string trackName;
+        [Tooltip("The artists for the track.")]
+        public string artists;
+    }
+
     public sealed class WwiseJukebox : JukeboxBase
     {
 
-        private Dictionary<int, bool> autoplayEnabled;
-        private Dictionary<int, string> wwiseLabels;
+        private bool[] autoplayEnabled;
         private bool isPlaying;
         private int currentTrack;
         private int[] currentShuffleOrder;
         private int currentShuffleIndex;
-
+        private uint wwiseEventID;
 
         [SerializeField] private JukeboxLoopMode defaultLoopMode = JukeboxLoopMode.StopAfterSongFinishes;
-        [SerializeField][SoundtrackID] private int[] tracks = null;
+        [SerializeField][SoundtrackID] private int wwiseEvent = 0;
+        [SerializeField] private SoundtrackMetaData[] tracks = null;
 
         private void Start()
         {
-            autoplayEnabled = new Dictionary<int, bool>();
-            foreach (int track in tracks)
-            {
-                autoplayEnabled.Add(track, true);
-                wwiseLabels.Add(track, SoundtrackSettings.Load().GetSetByID(track).name);
-            }
+            autoplayEnabled = new bool[tracks.Length];
+            for (int i = 0; i < tracks.Length; i++)
+                autoplayEnabled[i] = true;
+
+            SoundtrackSettings settings = SoundtrackSettings.Load();
+            SoundtrackSet even = settings.GetSetByID(wwiseEvent);
+
+            AkSoundEngine.SetState("Soundtrack", "Boss_CartoonMix");
+            wwiseEventID = AkSoundEngine.PostEvent(even.name, gameObject,
+                (uint)AkCallbackType.AK_EnableGetSourcePlayPosition,
+                WwiseCallback, null);
         }
 
         public override void Play()
@@ -94,12 +114,7 @@ namespace BattleRoyalRhythm.UI
 
         public override void ToggleTrackAutoplay(int trackID, bool isAutoPlay)
         {
-            if (autoplayEnabled.ContainsKey(trackID))
-                autoplayEnabled[trackID] = isAutoPlay;
 
-            uint beatMusicID = AkSoundEngine.PostEvent(wwiseLabels[trackID], gameObject,
-                (uint)AkCallbackType.AK_EnableGetSourcePlayPosition,
-                WwiseCallback, null);
         }
 
         private void WwiseCallback(object in_cookie, AkCallbackType in_type, AkCallbackInfo in_info)
